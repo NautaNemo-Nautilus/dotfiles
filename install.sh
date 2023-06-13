57b244c1b1c1cf7510f74e02632e63395c399185
bluegozila <mogidaiki@BluegozilaMacBook-Pro.local>
2023-06-14 00:35:11 +0900

    Correction required

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+DOT_DIR="$HOME/dotfiles"
+
+has() {
+    type "$1" > /dev/null 2>&1
+}
+
+if [ ! -d ${DOT_DIR} ]; then
+    if has "git"; then
+        git clone https://github.com/NautaNemo-Nautilus/dotfiles.git ${DOT_DIR}
+    else
+        echo "git required"
+        exit 1
+    fi
+    cd ${DOT_DIR}
+    for f in *
+    do
+        [[ "$f" == ".git" ]] && continue
+        [[ "$f" == ".gitignore" ]] && continue
+        [[ "$f" == ".DS_Store" ]] && continue
+        [[ "$f" == "README.md" ]] && continue
+        [[ "$f" == "install.sh" ]] && continue
+
+        ln -snf $DOT_DIR/"$f" $HOME/".$f"
+        echo "Installed .$f"
+    done
+fi
+
