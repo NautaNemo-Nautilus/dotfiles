d32b8b67742dff5e4571112786e613eff34977dc
bluegozila <mogidaiki@BluegozilaMacBook-Pro.local>
2023-06-14 00:35:45 +0900

    new config

---
+++ .config/fish/config-osx.fish
@@ -0,0 +1,7 @@
+#set -g theme_powerline_fonts no
+
+#mactex
+#export PATH=/usr/local/texlive/2023/bin/universal-darwin:$PATH
+
+
+
---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting ""
+
+#set -gx TERM alacritty
+set -gx EDITOR vim
+
+
+
+
+
+
+
+#theme
+#set -g theme_color_scheme terminal-dark
+set -g theme_display_git_default_branch yes
+
+
+switch (uname)
+	case Darwin
+		source (dirname (status --current-filename))/config-osx.fish
+	case Linux
+		source (dirname (status --current-filename))/config-linux.fish
+	case '*'
+		source (dirname (status --current-filename)).config-windows.fish
+end
