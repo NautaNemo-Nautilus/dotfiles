4303df91d0bf3ed65cf9a0bafd2350906bbbf900
bluegozila <mogidaiki@BluegozilaMacBook-Pro.local>
2023-08-31 15:49:03 +0900

    update

--- .config/fish/config-osx.fish
+++ .config/fish/config-osx.fish
@@ -1,6 +1,8 @@
 if status is-interactive
 	eval (/opt/homebrew/bin/brew shellenv)
+	set CPLUS_INCLUDE_PATH home/apg4b/include/bits/stdc++.h $CPLUS_INCLUDE_PATH
 end
+#set -x FONTCONFIG_PATH /opt/X11/lib/X11/fontconfig
 #set -g theme_powerline_fonts yes
 
 #mactex
